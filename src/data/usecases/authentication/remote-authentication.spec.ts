807ba3d7fd5c673349db2a509ca77d71dd71a87e
Leonardo Oliveira <leonfoliveira@gmail.com>
2020-12-12 20:21:34 -0300

    refactor: group imports in index files

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,13 +1,13 @@
 import faker from 'faker';
 
-import { HttpPostClientSpy } from '@/data/mocks/mock-http-client';
-import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error';
-import { HttpStatusCode } from '@/data/protocols/http/http-response';
-import { UnexpectedError } from '@/domain/errors/unexpected-error';
+import { HttpPostClientSpy } from '@/data/mocks';
+import { InvalidCredentialsError, UnexpectedError } from '@/domain/errors';
+import { HttpStatusCode } from '@/data/protocols/http';
 
-import { mockAccountModel, mockAuthentication } from '@/domain/mocks/mock-account';
-import { AuthenticationParams } from '@/domain/usecases/authentication';
-import { AccountModel } from '@/domain/models/account-model';
+import { mockAccountModel, mockAuthentication } from '@/domain/mocks';
+import { AuthenticationParams } from '@/domain/usecases';
+import { AccountModel } from '@/domain/models';
+
 import { RemoteAuthentication } from './remote-authentication';
 
 type SutTypes = {
--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,10 +1,8 @@
-import { Authentication, AuthenticationParams } from '@/domain/usecases/authentication';
-import { AccountModel } from '@/domain/models/account-model';
+import { Authentication, AuthenticationParams } from '@/domain/usecases';
+import { AccountModel } from '@/domain/models';
 
-import { HttpPostClient } from 'data/protocols/http/http-post-client';
-import { HttpStatusCode } from '@/data/protocols/http/http-response';
-import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error';
-import { UnexpectedError } from '@/domain/errors/unexpected-error';
+import { HttpPostClient, HttpStatusCode } from '@/data/protocols/http';
+import { InvalidCredentialsError, UnexpectedError } from '@/domain/errors';
 
 export class RemoteAuthentication implements Authentication {
   constructor(
