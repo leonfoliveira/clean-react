452935dcb9074133f599a67e873ba5905203767f
Leonardo Oliveira <leonfoliveira@gmail.com>
2020-12-12 20:05:26 -0300

    refactor: add generic params to HttpPostClient

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -6,15 +6,17 @@ import { HttpStatusCode } from '@/data/protocols/http/http-response';
 import { UnexpectedError } from '@/domain/errors/unexpected-error';
 
 import { mockAuthentication } from '@/domain/mocks/mock-authentication';
+import { AuthenticationParams } from '@/domain/usecases/authentication';
+import { AccountModel } from '@/domain/models/account-model';
 import { RemoteAuthentication } from './remote-authentication';
 
 type SutTypes = {
   sut: RemoteAuthentication;
-  httpPostClientSpy: HttpPostClientSpy;
+  httpPostClientSpy: HttpPostClientSpy<AuthenticationParams, AccountModel>;
 };
 
 const makeSut = (url: string = faker.internet.url()): SutTypes => {
-  const httpPostClientSpy = new HttpPostClientSpy();
+  const httpPostClientSpy = new HttpPostClientSpy<AuthenticationParams, AccountModel>();
   const sut = new RemoteAuthentication(url, httpPostClientSpy);
 
   return { sut, httpPostClientSpy };
--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,4 +1,5 @@
-import { AuthenticationParams } from 'domain/usecases/authentication';
+import { AuthenticationParams } from '@/domain/usecases/authentication';
+import { AccountModel } from '@/domain/models/account-model';
 
 import { HttpPostClient } from 'data/protocols/http/http-post-client';
 import { HttpStatusCode } from '@/data/protocols/http/http-response';
@@ -6,7 +7,10 @@ import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-err
 import { UnexpectedError } from '@/domain/errors/unexpected-error';
 
 export class RemoteAuthentication {
-  constructor(private readonly url: string, private readonly httpPostClient: HttpPostClient) {}
+  constructor(
+    private readonly url: string,
+    private readonly httpPostClient: HttpPostClient<AuthenticationParams, AccountModel>,
+  ) {}
 
   async auth(params: AuthenticationParams): Promise<void> {
     const httpResponse = await this.httpPostClient.post({ url: this.url, body: params });
