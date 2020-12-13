af25733802c1d94e00123a625b5f37d6aa6765a3
Leonardo Oliveira <leonfoliveira@gmail.com>
2020-12-13 15:23:38 -0300

    refactor: move input to a separate file

---
+++ src/presentation/components/input/input.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Styles from './input-styles.scss';
+
+type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>;
+const Input: React.FC<Props> = (props) => (
+  <div className={Styles.inputWrap}>
+    <input {...props} />
+    <span className={Styles.status} role="img" aria-label="invalid">
+      🔴
+    </span>
+  </div>
+);
+
+export default Input;
--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Header from '@/presentation/components/login-header/login-header';
+import Input from '@/presentation/components/input/input';
 import Spinner from '@/presentation/components/spinner/spinner';
 import Footer from '@/presentation/components/footer/footer';
 
@@ -11,22 +12,16 @@ const Login: React.FC = () => (
     <Header />
     <form className={Styles.form} action="">
       <h2>Login</h2>
-      <div className={Styles.inputWrap}>
-        <input type="email" name="email" placeholder="Digite seu email" />
-        <span className={Styles.status} role="img" aria-label="invalid">
-          🔴
-        </span>
-      </div>
-      <div className={Styles.inputWrap}>
-        <input type="password" name="password" placeholder="Digite sua senha" />
-        <span className={Styles.status} role="img" aria-label="invalid">
-          🔴
-        </span>
-      </div>
+      
+      <Input type="email" name="email" placeholder="Digite seu email" />
+      <Input type="password" name="password" placeholder="Digite sua senha" />
+
       <button className={Styles.submit} type="submit">
         Entrar
       </button>
+
       <span className={Styles.link}>Criar conta</span>
+
       <div className={Styles.errorWrap}>
         <Spinner className={Styles.spinner} />
         <span className={Styles.error}>Erro</span>
