98a32f9c42f2e6b26ab23bda0f34db2979ebd44a
Leonardo Oliveira <leonfoliveira@gmail.com>
2020-12-13 15:26:51 -0300

    refactor: move form status to a separate file

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Header from '@/presentation/components/login-header/login-header';
 import Input from '@/presentation/components/input/input';
-import Spinner from '@/presentation/components/spinner/spinner';
+import FormStatus from '@/presentation/components/form-status/form-status';
 import Footer from '@/presentation/components/footer/footer';
 
 import Styles from './login-styles.scss';
@@ -12,7 +12,7 @@ const Login: React.FC = () => (
     <Header />
     <form className={Styles.form} action="">
       <h2>Login</h2>
-      
+
       <Input type="email" name="email" placeholder="Digite seu email" />
       <Input type="password" name="password" placeholder="Digite sua senha" />
 
@@ -21,11 +21,7 @@ const Login: React.FC = () => (
       </button>
 
       <span className={Styles.link}>Criar conta</span>
-
-      <div className={Styles.errorWrap}>
-        <Spinner className={Styles.spinner} />
-        <span className={Styles.error}>Erro</span>
-      </div>
+      <FormStatus />
     </form>
     <Footer />
   </div>
