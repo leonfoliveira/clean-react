bc527848c33d6839c1aec2d67a37333b681203fb
Leonardo Oliveira <leonfoliveira@gmail.com>
2020-12-13 18:08:24 -0300

    feat: ensure Login present valid state if password is valid

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -18,9 +18,9 @@ const Input: React.FC<Props> = (props) => {
     setState({ ...state, [event.target.name]: event.target.value });
   };
 
-  const getStatus = (): string => '🔴';
+  const getStatus = (): string => (error ? '🔴' : '🟢');
 
-  const getTitle = (): string => error;
+  const getTitle = (): string => error || 'Tudo Certo!';
 
   return (
     <div className={Styles.inputWrap}>
