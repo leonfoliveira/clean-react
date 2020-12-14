d0086e1330b9a91288cf10988fcf021f29551a13
Leonardo Oliveira <leonfoliveira@gmail.com>
2020-12-14 11:43:59 -0300

    test: ensure Login navigates to main page on success

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -19,7 +19,7 @@ type SutParams = {
   validationError: string;
 };
 
-const history = createMemoryHistory();
+const history = createMemoryHistory({ initialEntries: ['/login'] });
 const makeSut = (params?: SutParams): SutTypes => {
   const validationStub = new ValidationStub();
   const authenticationSpy = new AuthenticationSpy();
@@ -185,6 +185,8 @@ describe('Login Component', () => {
     await waitFor(() => sut.getByTestId('form'));
 
     expect(localStorage.setItem).toHaveBeenCalledWith('accessToken', authenticationSpy.account.accessToken);
+    expect(history.length).toBe(1);
+    expect(history.location.pathname).toBe('/');
   });
 
   test('Should go to signup page', async () => {
