24d64c1728f77d89b3bbbbaf931d52c61eab93ce
Leonardo Oliveira <leonfoliveira@gmail.com>
2020-12-14 17:14:26 -0300

    refactor: move sut creation to a factory method

--- src/presentation/components/input/input.spec.tsx
+++ src/presentation/components/input/input.spec.tsx
@@ -1,29 +1,42 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, RenderResult } from '@testing-library/react';
 import Context from '@/presentation/contexts/login-form-context';
 
 import Input from './input';
 
+const makeSut = (): RenderResult =>
+  render(
+    <Context.Provider
+      value={{
+        state: {
+          isLoading: false,
+          email: '',
+          emailError: '',
+          password: '',
+          passwordError: '',
+          mainError: '',
+        },
+        setState: () => {},
+      }}
+    >
+      <Input name="field" />
+    </Context.Provider>,
+  );
+
 describe('Input Component', () => {
   test('Should begin with read only', () => {
-    const { getByTestId } = render(
-      <Context.Provider
-        value={{
-          state: {
-            isLoading: false,
-            email: '',
-            emailError: '',
-            password: '',
-            passwordError: '',
-            mainError: '',
-          },
-          setState: () => {},
-        }}
-      >
-        <Input name="field" />
-      </Context.Provider>,
-    );
+    const { getByTestId } = makeSut();
+
     const input = getByTestId('field') as HTMLInputElement;
+
+    expect(input.readOnly).toBe(true);
+  });
+
+  test('Should begin with read only', () => {
+    const { getByTestId } = makeSut();
+
+    const input = getByTestId('field') as HTMLInputElement;
+
     expect(input.readOnly).toBe(true);
   });
 });
