166fdb7f81b9e0b71ce32e75b5fc297671d47a40
Leonardo Oliveira <leonfoliveira@gmail.com>
2020-12-14 11:10:18 -0300

    test: prevent Login to call submit more than once

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -131,4 +131,15 @@ describe('Login Component', () => {
 
     expect(authenticationSpy.params).toEqual({ email, password });
   });
+
+  test('Should call Authentication only once', () => {
+    const { sut, authenticationSpy } = makeSut();
+    const email = faker.internet.email();
+    const password = faker.internet.password();
+
+    simulateValidSubmit(sut, email, password);
+    simulateValidSubmit(sut, email, password);
+
+    expect(authenticationSpy.callsCount).toBe(1);
+  });
 });
