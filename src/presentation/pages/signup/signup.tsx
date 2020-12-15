fe706ad46783dbaac61e04906cb26038f2502e80
Leonardo Oliveira <leonfoliveira@gmail.com>
2020-12-15 13:54:11 -0300

    test: ensure signup change passwordConfirmation status to error if validation fails

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -19,6 +19,7 @@ const Signup: React.FC<Props> = ({ validation }) => {
     emailError: '',
     password: '',
     passwordError: 'Campo Obrigatório',
+    passwordConfirmation: '',
     passwordConfirmationError: 'Campo Obrigatório',
     mainError: '',
   });
@@ -29,8 +30,12 @@ const Signup: React.FC<Props> = ({ validation }) => {
       nameError: validation.validate('name', state.name),
       emailError: validation.validate('email', state.email),
       passwordError: validation.validate('password', state.password),
+      passwordConfirmationError: validation.validate(
+        'passwordConfirmation',
+        state.passwordConfirmation,
+      ),
     });
-  }, [state.name, state.email, state.password]);
+  }, [state.name, state.email, state.password, state.passwordConfirmation]);
 
   return (
     <div className={Styles.signup}>
