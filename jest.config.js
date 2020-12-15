75da16e28a533bc52c53c235e342375cdb8ba4b0
Leonardo Oliveira <leonfoliveira@gmail.com>
2020-12-15 17:23:07 -0300

    chore: add cypress

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,7 @@ module.exports = {
     '!**/*.d.ts',
   ],
   coverageDirectory: 'coverage',
+  testPathIgnorePatterns: ['<rootDir>/node_modules', '<rootDir>/src/main/test/cypress'],
   testEnvironment: 'jsdom',
   transform: {
     '.+\\.(ts|tsx)$': 'ts-jest',
