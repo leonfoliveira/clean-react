8c93e7f0ca050637965716bf79d2d247c5844823
Leonardo Oliveira <leonfoliveira@gmail.com>
2020-12-15 18:29:21 -0300

    test: ensure Login save accessToken if valid credentials are provided

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,5 +1,7 @@
 import faker from 'faker';
 
+const { baseUrl } = Cypress.config();
+
 describe('Login', () => {
   beforeEach(() => {
     cy.visit('login');
@@ -29,5 +31,49 @@ describe('Login', () => {
       .should('contain.text', '🔴');
     cy.getByTestId('submit').should('have.attr', 'disabled');
     cy.getByTestId('error-wrap').should('not.have.descendants');
+  });
+
+  it('Should present valid state if form is valid', () => {
+    cy.getByTestId('email').focus().type(faker.internet.email());
+    cy.getByTestId('email-status')
+      .should('have.attr', 'title', 'Tudo Certo!')
+      .should('contain.text', '🟢');
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(5));
+    cy.getByTestId('password-status')
+      .should('have.attr', 'title', 'Tudo Certo!')
+      .should('contain.text', '🟢');
+    cy.getByTestId('submit').should('not.have.attr', 'disabled');
+    cy.getByTestId('error-wrap').should('not.have.descendants');
+  });
+
+  it('Should present error if invalid credentials are provided', () => {
+    cy.getByTestId('email').focus().type(faker.internet.email());
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(5));
+    cy.getByTestId('submit').click();
+    cy.getByTestId('error-wrap')
+      .getByTestId('spinner')
+      .should('exist')
+      .getByTestId('main-error')
+      .should('not.exist')
+      .getByTestId('spinner')
+      .should('not.exist')
+      .getByTestId('main-error')
+      .should('contain.text', 'Invalid Credentials');
+    cy.url().should('equal', `${baseUrl}/login`);
+  });
+
+  it('Should present save accessToken if valid credentials are provided', () => {
+    cy.getByTestId('email').focus().type('mango@gmail.com');
+    cy.getByTestId('password').focus().type('12345');
+    cy.getByTestId('submit').click();
+    cy.getByTestId('error-wrap')
+      .getByTestId('spinner')
+      .should('exist')
+      .getByTestId('main-error')
+      .should('not.exist')
+      .getByTestId('spinner')
+      .should('not.exist');
+    cy.url().should('equal', `${baseUrl}/`);
+    cy.window().then((window) => assert.isOk(window.localStorage.getItem('accessToken')));
   });
 });
