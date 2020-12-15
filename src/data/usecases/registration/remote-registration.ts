675e0aeca7577bb780184edbbf4e39701cf75dcf
Leonardo Oliveira <leonfoliveira@gmail.com>
2020-12-15 11:59:52 -0300

    feat: ensure HttpPostClient returns an AccountModel when success

--- src/data/usecases/registration/remote-registration.ts
+++ src/data/usecases/registration/remote-registration.ts
@@ -1,14 +1,20 @@
-import { HttpPostClient } from '@/data/protocols/http';
+import { HttpPostClient, HttpStatusCode } from '@/data/protocols/http';
 import { AccountModel } from '@/domain/models';
-import { RegistrationParams } from '@/domain/usecases/registration';
+import { Registration, RegistrationParams } from '@/domain/usecases/registration';
 
-export class RemoteRegistration {
+export class RemoteRegistration implements Registration {
   constructor(
     private readonly url: string,
     private readonly httpPostClient: HttpPostClient<RegistrationParams, AccountModel>,
   ) {}
 
-  async register(params: RegistrationParams): Promise<void> {
-    await this.httpPostClient.post({ url: this.url, body: params });
+  async register(params: RegistrationParams): Promise<AccountModel> {
+    const httpResponse = await this.httpPostClient.post({ url: this.url, body: params });
+
+    switch (httpResponse.statusCode) {
+      default:
+      case HttpStatusCode.ok:
+        return httpResponse.body;
+    }
   }
 }
