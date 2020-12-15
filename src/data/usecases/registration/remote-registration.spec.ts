0078602c5a989818a5712d3ffdaf40410a029bd6
Leonardo Oliveira <leonfoliveira@gmail.com>
2020-12-15 11:41:52 -0300

    test: ensure Registration calls HttpPostClient with correct URL

---
+++ src/data/usecases/registration/remote-registration.spec.ts
@@ -0,0 +1,21 @@
+import { HttpPostClientSpy } from '@/data/mocks';
+import { AccountModel } from '@/domain/models';
+import { RegistrationParams } from '@/domain/usecases/registration';
+
+import { RemoteRegistration } from './remote-registration';
+
+describe('Registration', () => {
+  test('Should call HttpPostClient with correct URL', () => {
+    const httpPostClientSpy = new HttpPostClientSpy<RegistrationParams, AccountModel>();
+    const sut = new RemoteRegistration('any_url', httpPostClientSpy);
+
+    sut.register({
+      name: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+    });
+
+    expect(httpPostClientSpy.url).toBe('any_url');
+  });
+});
