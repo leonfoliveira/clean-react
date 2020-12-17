5dd3890e5ff933154cd905507c74fc1535a98108
Leonardo Oliveira <leonfoliveira@gmail.com>
2020-12-17 17:23:36 -0300

    feat: ensure SurveyList renders 4 empty LIs on start

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 
 import { Footer, Header } from '@/presentation/components';
+import { SurveyItemEmpty } from '@/presentation/pages/survey-list/components';
 
 import Styles from './survey-list-styles.scss';
 
@@ -10,7 +11,9 @@ const SurveyList: React.FC = () => (
     <Header />
     <div className={Styles.contentWrap}>
       <h2>Enquetes</h2>
-      <ul />
+      <ul data-testid="survey-list">
+        <SurveyItemEmpty />
+      </ul>
     </div>
     <Footer />
   </div>
