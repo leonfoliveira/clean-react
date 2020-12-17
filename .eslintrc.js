159f3bfacfb3370e147ba7725d098e94630c224b
Leonardo Oliveira <leonfoliveira@gmail.com>
2020-12-17 17:40:14 -0300

    chore: disable a11y rules

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+const a11yOff = Object.keys(require('eslint-plugin-jsx-a11y').rules).reduce((acc, rule) => {
+  acc[`jsx-a11y/${rule}`] = 'off';
+  return acc;
+}, {});
+
+module.exports = {
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  extends: [
+    'airbnb-typescript',
+    'plugin:react/recommended',
+    'plugin:cypress/recommended',
+    'prettier',
+  ],
+  env: {
+    'cypress/globals': true,
+  },
+  plugins: ['react', 'cypress'],
+  parserOptions: {
+    project: './tsconfig.lint.json',
+  },
+  rules: {
+    ...a11yOff,
+    'class-methods-use-this': 'off',
+    'max-classes-per-file': 'off',
+    'import/prefer-default-export': 'off',
+    'import/no-extraneous-dependencies': [
+      'error',
+      { devDependencies: ['**/test/**', '**/*.test.*', '**/*.spec.*'] },
+    ],
+    'react/jsx-props-no-spreading': 'off',
+    'react/prop-types': 'off',
+    'react/destructuring-assignment': 'off',
+    '@typescript-eslint/lines-between-class-members': [
+      'error',
+      'always',
+      { exceptAfterSingleLine: true },
+    ],
+    '@typescript-eslint/indent': 'off',
+  },
+};
