f7ec75a6c83ad57f4da45cda32267b37b9c50ec4
Leonardo Oliveira <leonfoliveira@gmail.com>
2020-12-17 21:24:20 -0300

    fix: correct sass files imports

--- src/presentation/pages/survey-list/components/list/components/item/item.tsx
+++ src/presentation/pages/survey-list/components/list/components/item/item.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { SurveyModel } from '@/domain/models';
 import { Icon, IconName } from '@/presentation/components';
 
-import Styles from './survey-item-styles.scss';
+import Styles from './item-styles.scss';
 
 type Props = {
   survey: SurveyModel;
