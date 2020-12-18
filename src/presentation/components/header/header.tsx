7c6578b7a7374498cc3e5009c21f4347aac73dfa
Leonardo Oliveira <leonfoliveira@gmail.com>
2020-12-18 17:23:18 -0300

    feat: ensure logout calls setCurrentAccount with null

--- src/presentation/components/header/header.tsx
+++ src/presentation/components/header/header.tsx
@@ -1,20 +1,36 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { memo } from 'react';
+import React, { memo, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
 
+import { ApiContext } from '@/presentation/contexts';
 import { Logo } from '@/presentation/components';
 
 import Styles from './header-styles.scss';
 
-const Header: React.FC = () => (
-  <header className={Styles.headerWrap}>
-    <div className={Styles.headerContent}>
-      <Logo />
-      <div className={Styles.logoutWrap}>
-        <span>Leonardo</span>
-        <a href="#">Sair</a>
+const Header: React.FC = () => {
+  const history = useHistory();
+  const { setCurrentAccount } = useContext(ApiContext);
+
+  const logout = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>): void => {
+    event.preventDefault();
+
+    setCurrentAccount(null);
+    history.replace('/login');
+  };
+
+  return (
+    <header className={Styles.headerWrap}>
+      <div className={Styles.headerContent}>
+        <Logo />
+        <div className={Styles.logoutWrap}>
+          <span>Leonardo</span>
+          <a href="#" onClick={logout} data-testid="logout">
+            Sair
+          </a>
+        </div>
       </div>
-    </div>
-  </header>
-);
+    </header>
+  );
+};
 
 export default memo(Header);
