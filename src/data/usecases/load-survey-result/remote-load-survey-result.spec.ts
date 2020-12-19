0796c9c89042dbed8058e401d1d8958c7541659e
Leonardo Oliveira <leonfoliveira@gmail.com>
2020-12-19 18:18:35 -0300

    test: ensure RemoteSurveyResult throw UnexpectedError on 500

--- src/data/usecases/load-survey-result/remote-load-survey-result.spec.ts
+++ src/data/usecases/load-survey-result/remote-load-survey-result.spec.ts
@@ -50,4 +50,14 @@ describe('RemoteSurveyResult', () => {
     const promise = sut.load();
     await expect(promise).rejects.toThrow(new UnexpectedError());
   });
+
+  test('Should throw UnexpectedError if HttpGetClient returns 500', async () => {
+    const { sut, httpGetClientSpy } = makeSut();
+    httpGetClientSpy.response = {
+      statusCode: HttpStatusCode.serverError,
+    };
+
+    const promise = sut.load();
+    await expect(promise).rejects.toThrow(new UnexpectedError());
+  });
 });
