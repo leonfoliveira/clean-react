d1c5b07143094ab2fe87a7caac1014d249fe359a
Leonardo Oliveira <leonfoliveira@gmail.com>
2020-12-19 18:14:27 -0300

    test: ensure RemoteSurveyResult throw AccessDeniedError on 403

--- src/data/usecases/load-survey-result/remote-load-survey-result.spec.ts
+++ src/data/usecases/load-survey-result/remote-load-survey-result.spec.ts
@@ -1,7 +1,9 @@
 import faker from 'faker';
 
 import { HttpGetClientSpy } from '@/data/test';
+import { HttpStatusCode } from '@/data/protocols/http';
 
+import { AccessDeniedError } from '@/domain/errors';
 import { RemoteLoadSurveyResult } from './remote-load-survey-result';
 
 type SutTypes = {
@@ -27,5 +29,15 @@ describe('RemoteSurveyResult', () => {
     await sut.load();
 
     expect(httpGetClientSpy.url).toBe(url);
+  });
+
+  test('Should throw AccessDeniedError if HttpGetClient returns 403', async () => {
+    const { sut, httpGetClientSpy } = makeSut();
+    httpGetClientSpy.response = {
+      statusCode: HttpStatusCode.forbidden,
+    };
+
+    const promise = sut.load();
+    await expect(promise).rejects.toThrow(new AccessDeniedError());
   });
 });
