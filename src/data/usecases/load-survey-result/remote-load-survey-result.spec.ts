defc9935431404a60d69e1ef2a2ee67d964f71a4
Leonardo Oliveira <leonfoliveira@gmail.com>
2020-12-19 18:05:19 -0300

    test: ensure RemoteSurveyResult calls HttpGetClient with correct URL

---
+++ src/data/usecases/load-survey-result/remote-load-survey-result.spec.ts
@@ -0,0 +1,17 @@
+import faker from 'faker';
+
+import { HttpGetClientSpy } from '@/data/test';
+
+import { RemoteLoadSurveyResult } from './remote-load-survey-result';
+
+describe('RemoteSurveyResult', () => {
+  test('Should call HttpGetClient with correct URL', async () => {
+    const url = faker.internet.url();
+    const httpGetClientSpy = new HttpGetClientSpy();
+    const sut = new RemoteLoadSurveyResult(url, httpGetClientSpy);
+
+    await sut.load();
+
+    expect(httpGetClientSpy.url).toBe(url);
+  });
+});
