f8dab3547ebf4e7e70898b736e93896a85e1a49c
Leonardo Oliveira <leonfoliveira@gmail.com>
2020-12-19 14:04:26 -0300

    fix: change compare fields validation test

--- src/validation/validators/compare-fields/compare-fields-validation.spec.ts
+++ src/validation/validators/compare-fields/compare-fields-validation.spec.ts
@@ -8,8 +8,8 @@ const makeSut = (field: string, fieldToCompare: string): CompareFieldsValidation
 
 describe('CompareFieldsValidation', () => {
   test('Should return error if compare is invalid', () => {
-    const field = faker.database.column();
-    const fieldToCompare = faker.database.column();
+    const field = 'any_field';
+    const fieldToCompare = 'other_field';
     const sut = makeSut(field, fieldToCompare);
 
     const error = sut.validate({
@@ -21,8 +21,8 @@ describe('CompareFieldsValidation', () => {
   });
 
   test('Should return falsy if compare is valid', () => {
-    const field = faker.database.column();
-    const fieldToCompare = faker.database.column();
+    const field = 'any_field';
+    const fieldToCompare = 'other_field';
     const value = faker.random.word();
     const sut = makeSut(field, fieldToCompare);
 
