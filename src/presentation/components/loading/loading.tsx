aea5f5569508f4ffa4752579154f4744190a9978
Leonardo Oliveira <leonfoliveira@gmail.com>
2020-12-19 19:22:00 -0300

    refactor: move Loading to its own file

---
+++ src/presentation/components/loading/loading.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { Spinner } from '@/presentation/components';
+
+import Styles from './loading-styles.scss';
+
+const Loading: React.FC = () => (
+  <div className={Styles.loadingWrap}>
+    <div className={Styles.loading}>
+      <span>Aguarde...</span>
+      <Spinner className={Styles.spinner} />
+    </div>
+  </div>
+);
+
+export default Loading;
