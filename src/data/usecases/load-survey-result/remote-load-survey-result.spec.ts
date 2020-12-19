ade8801fda9c6f659a722a01211fe3c54a5efd64
Leonardo Oliveira <leonfoliveira@gmail.com>
2020-12-19 18:08:34 -0300

    refactor: move SUT creation to a factory method

--- src/data/usecases/load-survey-result/remote-load-survey-result.spec.ts
+++ src/data/usecases/load-survey-result/remote-load-survey-result.spec.ts
@@ -4,11 +4,25 @@ import { HttpGetClientSpy } from '@/data/test';
 
 import { RemoteLoadSurveyResult } from './remote-load-survey-result';
 
+type SutTypes = {
+  sut: RemoteLoadSurveyResult;
+  httpGetClientSpy: HttpGetClientSpy<RemoteLoadSurveyResult.Model>;
+};
+
+const makeSut = (url = faker.internet.url()): SutTypes => {
+  const httpGetClientSpy = new HttpGetClientSpy<RemoteLoadSurveyResult.Model>();
+  const sut = new RemoteLoadSurveyResult(url, httpGetClientSpy);
+
+  return {
+    sut,
+    httpGetClientSpy,
+  };
+};
+
 describe('RemoteSurveyResult', () => {
   test('Should call HttpGetClient with correct URL', async () => {
     const url = faker.internet.url();
-    const httpGetClientSpy = new HttpGetClientSpy();
-    const sut = new RemoteLoadSurveyResult(url, httpGetClientSpy);
+    const { sut, httpGetClientSpy } = makeSut(url);
 
     await sut.load();
 
--- src/data/usecases/load-survey-result/remote-load-survey-result.ts
+++ src/data/usecases/load-survey-result/remote-load-survey-result.ts
@@ -1,9 +1,17 @@
 import { HttpGetClient } from '@/data/protocols/http';
+import { LoadSurveyResult } from '@/domain/usecases';
 
 export class RemoteLoadSurveyResult {
-  constructor(private readonly url: string, private readonly httpGetClient: HttpGetClient) {}
+  constructor(
+    private readonly url: string,
+    private readonly httpGetClient: HttpGetClient<RemoteLoadSurveyResult.Model>,
+  ) {}
 
   async load(): Promise<void> {
     this.httpGetClient.get({ url: this.url });
   }
+}
+
+export namespace RemoteLoadSurveyResult {
+  export type Model = LoadSurveyResult.Model;
 }
