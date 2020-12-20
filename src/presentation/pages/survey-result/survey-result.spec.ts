637e3c463b4dcd62694930b0b15b56b0c927beb2
Leonardo Oliveira <leonfoliveira@gmail.com>
2020-12-20 19:18:12 -0300

    fix: change test to prevent multiple clicks

--- src/presentation/pages/survey-result/survey-result.spec.ts
+++ src/presentation/pages/survey-result/survey-result.spec.ts
@@ -5,8 +5,10 @@ import { LoadSurveyResultSpy, SaveSurveyResultSpy, mockSurveyResultModel } from 
 import { AccessDeniedError, UnexpectedError } from '@/domain/errors';
 import { AccountModel } from '@/domain/models';
 import { renderWithHistory } from '@/presentation/test';
+import { LoadSurveyResult } from '@/domain/usecases';
 
 import SurveyResult from './survey-result';
+import { surveyResultState } from './components';
 
 type SutTypes = {
   loadSurveyResultSpy: LoadSurveyResultSpy;
@@ -18,11 +20,18 @@ type SutTypes = {
 type SutParams = {
   loadSurveyResultSpy?: LoadSurveyResultSpy;
   saveSurveyResultSpy?: SaveSurveyResultSpy;
+  initialState?: {
+    isLoading: boolean;
+    error: string;
+    surveyResult: LoadSurveyResult.Model;
+    reload: boolean;
+  };
 };
 
 const makeSut = ({
   loadSurveyResultSpy = new LoadSurveyResultSpy(),
   saveSurveyResultSpy = new SaveSurveyResultSpy(),
+  initialState = null,
 }: SutParams = {}): SutTypes => {
   const history = createMemoryHistory({
     initialEntries: ['/', '/surveys/any_id'],
@@ -36,6 +45,7 @@ const makeSut = ({
         loadSurveyResult: loadSurveyResultSpy,
         saveSurveyResult: saveSurveyResultSpy,
       }),
+    states: initialState ? [{ atom: surveyResultState, value: initialState }] : [],
   });
 
   return { loadSurveyResultSpy, saveSurveyResultSpy, history, setCurrentAccountMock };
@@ -224,14 +234,19 @@ describe('SurveyResult Component', () => {
   });
 
   test('Should prevent multiple answer click', async () => {
-    const { saveSurveyResultSpy } = makeSut();
+    const initialState = {
+      isLoading: true,
+      error: '',
+      surveyResult: null as LoadSurveyResult.Model,
+      reload: false,
+    };
+    const { saveSurveyResultSpy } = makeSut({ initialState });
 
     await waitFor(() => screen.getByTestId('survey-result'));
     const answersWrap = screen.queryAllByTestId('answer-wrap');
     fireEvent.click(answersWrap[1]);
     await waitFor(() => screen.getByTestId('survey-result'));
-    fireEvent.click(answersWrap[1]);
-    await waitFor(() => screen.getByTestId('survey-result'));
-    expect(saveSurveyResultSpy.callsCount).toBe(1);
+
+    expect(saveSurveyResultSpy.callsCount).toBe(0);
   });
 });
--- src/presentation/test/render-helper.tsx
+++ src/presentation/test/render-helper.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Router } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import { MemoryHistory } from 'history';
-import { RecoilRoot } from 'recoil';
+import { MutableSnapshot, RecoilRoot, RecoilState } from 'recoil';
 
 import { mockAccountModel } from '@/domain/test';
 import { currentAccountState } from '@/presentation/components';
@@ -12,6 +12,10 @@ type Params = {
   Page: React.FC;
   history: MemoryHistory;
   account?: AccountModel;
+  states?: {
+    atom: RecoilState<any>;
+    value: any;
+  }[];
 };
 
 type Result = {
@@ -23,18 +27,22 @@ export const renderWithHistory = ({
   Page,
   history,
   account = mockAccountModel(),
+  states = [],
 }: Params): Result => {
   const setCurrentAccountMock = jest.fn();
   const getCurrentAccountMock = () => account;
+  const mockedState = {
+    setCurrentAccount: setCurrentAccountMock,
+    getCurrentAccount: getCurrentAccountMock,
+  };
+  const initializeState = ({ set }: MutableSnapshot): void => {
+    [...states, { atom: currentAccountState, value: mockedState }].forEach((state) =>
+      set(state.atom, state.value),
+    );
+  };
+
   render(
-    <RecoilRoot
-      initializeState={({ set }) => {
-        set(currentAccountState, {
-          setCurrentAccount: setCurrentAccountMock,
-          getCurrentAccount: getCurrentAccountMock,
-        });
-      }}
-    >
+    <RecoilRoot initializeState={initializeState}>
       <Router history={history}>
         <Page />
       </Router>
