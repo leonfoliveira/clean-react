ec6c5356578780cbd966c28afb2bafe858cbfa48
Leonardo Oliveira <leonfoliveira@gmail.com>
2020-12-20 12:06:57 -0300

    test: ensure SurveyResult presents correct data on success

--- src/main/test/cypress/integration/survey-result.test.ts
+++ src/main/test/cypress/integration/survey-result.test.ts
@@ -4,7 +4,7 @@ import { Interceptor } from '../utils/interceptor';
 const path = /surveys\/(.+)\/results/;
 const mockUnexpectedError = () => Interceptor.mockCustomErrors('GET', path, [404, 500]);
 const mockAccessDeniedError = () => Interceptor.mockForbidden('GET', path);
-// const mockSuccess = () => Interceptor.mockOk('GET', path, { fixture: 'survey-list' });
+const mockSuccess = () => Interceptor.mockOk('GET', path, { fixture: 'survey-result' });
 
 describe('SurveyResult', () => {
   beforeEach(() => {
@@ -39,5 +39,25 @@ describe('SurveyResult', () => {
     mockAccessDeniedError();
     cy.visit('/surveys/any_id');
     Helpers.testUrl('/login');
+  });
+
+  it('Should present survey result', () => {
+    mockSuccess();
+    cy.visit('/surveys/any_id');
+    cy.getByTestId('question').should('have.text', 'Question');
+    cy.getByTestId('day').should('have.text', '03');
+    cy.getByTestId('month').should('have.text', 'fev');
+    cy.getByTestId('year').should('have.text', '2018');
+    cy.get('li:nth-child(1)').then((li) => {
+      assert.isNotEmpty(li.attr('class'));
+      assert.equal(li.find('[data-testid="answer"]').text(), 'any_answer');
+      assert.equal(li.find('[data-testid="image"]').attr('src'), 'any_image');
+      assert.equal(li.find('[data-testid="percent"]').text(), '70%');
+    });
+    cy.get('li:nth-child(2)').then((li) => {
+      assert.equal(li.find('[data-testid="answer"]').text(), 'any_answer_2');
+      assert.notExists(li.find('[data-testid="image"]'));
+      assert.equal(li.find('[data-testid="percent"]').text(), '30%');
+    });
   });
 });
