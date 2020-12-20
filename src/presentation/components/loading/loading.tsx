0c57b86a5d6668d4995d2695dc33d1ccac109f18
Leonardo Oliveira <leonfoliveira@gmail.com>
2020-12-19 21:28:00 -0300

    feat: ensure SurveyResult presents correct initial state

--- src/presentation/components/loading/loading.tsx
+++ src/presentation/components/loading/loading.tsx
@@ -5,7 +5,7 @@ import { Spinner } from '@/presentation/components';
 import Styles from './loading-styles.scss';
 
 const Loading: React.FC = () => (
-  <div className={Styles.loadingWrap}>
+  <div className={Styles.loadingWrap} data-testid="loading">
     <div className={Styles.loading}>
       <span>Aguarde...</span>
       <Spinner className={Styles.spinner} />
--- src/presentation/pages/survey-result/survey-result.tsx
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -1,43 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FlipMove from 'react-flip-move';
 
-import { Calendar, Footer, Header } from '@/presentation/components';
+import { Calendar, Error, Footer, Header, Loading } from '@/presentation/components';
+import { LoadSurveyResult } from '@/domain/usecases';
 
 import Styles from './survey-result-styles.scss';
 
-const SurveyResult: React.FC = () => (
-  <div className={Styles.surveyResultWrap}>
-    <Header />
-    <div className={Styles.contentWrap}>
-      {true && (
-        <>
-          <hgroup>
-            <Calendar date={new Date()} className={Styles.calendarWrap} />
-            <h2>Qual é seu framework web favorito?</h2>
-          </hgroup>
-          <FlipMove className={Styles.answersList}>
-            <li>
-              <img src="http://fordevs.herokuapp.com/static/img/logo-react.png" />
-              <span className={Styles.answer}>ReactJS</span>
-              <span className={Styles.percent}>50%</span>
-            </li>
-            <li className={Styles.active}>
-              <img src="http://fordevs.herokuapp.com/static/img/logo-react.png" />
-              <span className={Styles.answer}>ReactJS</span>
-              <span className={Styles.percent}>50%</span>
-            </li>
-            <li>
-              <img src="http://fordevs.herokuapp.com/static/img/logo-react.png" />
-              <span className={Styles.answer}>ReactJS</span>
-              <span className={Styles.percent}>50%</span>
-            </li>
-          </FlipMove>
-          <button type="button">Voltar</button>
-        </>
-      )}
+const SurveyResult: React.FC = () => {
+  const [state] = useState({
+    isLoading: false,
+    error: '',
+    surveyResult: null as LoadSurveyResult.Model,
+  });
+
+  return (
+    <div className={Styles.surveyResultWrap}>
+      <Header />
+      <div className={Styles.contentWrap} data-testid="survey-result">
+        {state.surveyResult && (
+          <>
+            <hgroup>
+              <Calendar date={new Date()} className={Styles.calendarWrap} />
+              <h2>Qual é seu framework web favorito?</h2>
+            </hgroup>
+            <FlipMove className={Styles.answersList}>
+              <li>
+                <img src="http://fordevs.herokuapp.com/static/img/logo-react.png" />
+                <span className={Styles.answer}>ReactJS</span>
+                <span className={Styles.percent}>50%</span>
+              </li>
+              <li className={Styles.active}>
+                <img src="http://fordevs.herokuapp.com/static/img/logo-react.png" />
+                <span className={Styles.answer}>ReactJS</span>
+                <span className={Styles.percent}>50%</span>
+              </li>
+              <li>
+                <img src="http://fordevs.herokuapp.com/static/img/logo-react.png" />
+                <span className={Styles.answer}>ReactJS</span>
+                <span className={Styles.percent}>50%</span>
+              </li>
+            </FlipMove>
+            <button type="button">Voltar</button>
+          </>
+        )}
+        {state.isLoading && <Loading />}
+        {state.error && <Error error={state.error} handleClick={() => {}} />}
+      </div>
+      <Footer />
     </div>
-    <Footer />
-  </div>
-);
+  );
+};
 
 export default SurveyResult;
