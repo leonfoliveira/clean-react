b7a8a19dbf2e5be6bee9d6ce0733596993ad6c1f
Leonardo Oliveira <leonfoliveira@gmail.com>
2020-12-20 14:58:24 -0300

    test: ensure SurveyResult presents error if SaveSurveyResult fails

--- src/presentation/pages/survey-result/survey-result.spec.tsx
+++ src/presentation/pages/survey-result/survey-result.spec.tsx
@@ -169,4 +169,19 @@ describe('SurveyResult Component', () => {
       answer: loadSurveyResultSpy.surveyResult.answers[1].answer,
     });
   });
+
+  test('Should render error on unexpectedError', async () => {
+    const saveSurveyResultSpy = new SaveSurveyResultSpy();
+    const error = new UnexpectedError();
+    jest.spyOn(saveSurveyResultSpy, 'save').mockRejectedValueOnce(error);
+    makeSut({ saveSurveyResultSpy });
+
+    await waitFor(() => screen.getByTestId('survey-result'));
+    const answersWrap = screen.queryAllByTestId('answer-wrap');
+    fireEvent.click(answersWrap[1]);
+    await waitFor(() => screen.getByTestId('survey-result'));
+    expect(screen.queryByTestId('question')).not.toBeInTheDocument();
+    expect(screen.getByTestId('error')).toHaveTextContent(error.message);
+    expect(screen.queryByTestId('loading')).not.toBeInTheDocument();
+  });
 });
